4ac3bd895b753c3d98825eb369d5e9188bf2eac6
ALTERIO <146441318+jeanlee0990@users.noreply.github.com>
2024-03-27 23:19:17 +0700

    Add files via upload

---
+++ pertemuan7.php
@@ -0,0 +1,16 @@
+<?php
+function displayTitle($title) {
+    echo "<h2>$title</h2>";
+}
+
+function displayMenu() {
+    echo "[1] Penggunaan if<br>";
+    echo "[2] Penggunaan switch ... case<br>";
+    echo "[3] Penggunaan looping<br>";
+    echo "[4] Penggunaan array<br>";
+}
+
+displayTitle("Materi Pemrograman PHP");
+
+displayMenu();
+?>
